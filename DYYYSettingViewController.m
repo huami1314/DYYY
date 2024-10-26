bfb005a3d85444ca5aa1876096ab1a6f077ea15f
huami1314 <huami1214@email.cn>
2024-10-27 03:35:33 +0800

    v1.6-2

--- DYYYSettingViewController.m
+++ DYYYSettingViewController.m
@@ -1,42 +1,59 @@
 #import "DYYYSettingViewController.h"
 
+typedef NS_ENUM(NSInteger, DYYYSettingItemType) {
+    DYYYSettingItemTypeSwitch,
+    DYYYSettingItemTypeTextField,
+    DYYYSettingItemTypeSpeedPicker // 新增的类型
+};
+
+@interface DYYYSettingItem : NSObject
+
+@property (nonatomic, copy) NSString *title;
+@property (nonatomic, copy) NSString *key;
+@property (nonatomic, assign) DYYYSettingItemType type;
+@property (nonatomic, copy, nullable) NSString *placeholder;
+
++ (instancetype)itemWithTitle:(NSString *)title key:(NSString *)key type:(DYYYSettingItemType)type;
++ (instancetype)itemWithTitle:(NSString *)title key:(NSString *)key type:(DYYYSettingItemType)type placeholder:(nullable NSString *)placeholder;
+
+@end
+
+@implementation DYYYSettingItem
+
++ (instancetype)itemWithTitle:(NSString *)title key:(NSString *)key type:(DYYYSettingItemType)type {
+    return [self itemWithTitle:title key:key type:type placeholder:nil];
+}
+
++ (instancetype)itemWithTitle:(NSString *)title key:(NSString *)key type:(DYYYSettingItemType)type placeholder:(nullable NSString *)placeholder {
+    DYYYSettingItem *item = [[DYYYSettingItem alloc] init];
+    item.title = title;
+    item.key = key;
+    item.type = type;
+    item.placeholder = placeholder;
+    return item;
+}
+
+@end
+
 @interface DYYYSettingViewController () <UITableViewDelegate, UITableViewDataSource>
+
 @property (nonatomic, strong) UITableView *tableView;
 @property (nonatomic, strong) NSArray<NSArray<DYYYSettingItem *> *> *settingSections;
 @property (nonatomic, strong) UILabel *footerLabel;
-@property (nonatomic, assign) BOOL isDanmuColorEnabled;
+
 @end
 
 @implementation DYYYSettingViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
     self.title = @"设置";
     [self setupAppearance];
     [self setupTableView];
     [self setupSettingItems];
     [self setupFooterLabel];
     [self addTitleGradientAnimation];
-
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
-}
-
-- (void)dealloc {
-    [[NSNotificationCenter defaultCenter] removeObserver:self];
-}
-
-- (void)keyboardWillShow:(NSNotification *)notification {
-    CGRect keyboardFrame = [notification.userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue];
-    UIEdgeInsets contentInsets = UIEdgeInsetsMake(0.0, 0.0, keyboardFrame.size.height, 0.0);
-    self.tableView.contentInset = contentInsets;
-    self.tableView.scrollIndicatorInsets = contentInsets;
-}
-
-- (void)keyboardWillHide:(NSNotification *)notification {
-    UIEdgeInsets contentInsets = UIEdgeInsetsZero;
-    self.tableView.contentInset = contentInsets;
-    self.tableView.scrollIndicatorInsets = contentInsets;
 }
 
 - (void)setupAppearance {
@@ -58,14 +75,11 @@
 }
 
 - (void)setupSettingItems {
-    self.isDanmuColorEnabled = [[NSUserDefaults standardUserDefaults] boolForKey:@"DYYYEnableDanmuColor"];
-    NSMutableArray *section4 = [NSMutableArray arrayWithObject:[DYYYSettingItem itemWithTitle:@"开启弹幕改色" key:@"DYYYEnableDanmuColor" type:DYYYSettingItemTypeSwitch]];
-    
-    if (self.isDanmuColorEnabled) {
-        [section4 addObject:[DYYYSettingItem itemWithTitle:@"修改弹幕颜色" key:@"DYYYdanmuColor" type:DYYYSettingItemTypeTextField placeholder:@"十六进制颜色"]];
-    }
-    
     self.settingSections = @[
+        @[
+            [DYYYSettingItem itemWithTitle:@"开启弹幕改色" key:@"DYYYEnableDanmuColor" type:DYYYSettingItemTypeSwitch],
+            [DYYYSettingItem itemWithTitle:@"修改弹幕颜色" key:@"DYYYdanmuColor" type:DYYYSettingItemTypeTextField placeholder:@"十六进制"]
+        ],
         @[
             [DYYYSettingItem itemWithTitle:@"设置顶栏透明" key:@"DYYYtopbartransparent" type:DYYYSettingItemTypeTextField placeholder:@"输入0-1的小数"],
             [DYYYSettingItem itemWithTitle:@"设置全局透明" key:@"DYYYGlobalTransparency" type:DYYYSettingItemTypeTextField placeholder:@"输入0-1的小数"],
@@ -82,8 +96,7 @@
             [DYYYSettingItem itemWithTitle:@"隐藏评论按钮" key:@"DYYYHideCommentButton" type:DYYYSettingItemTypeSwitch],
             [DYYYSettingItem itemWithTitle:@"隐藏收藏按钮" key:@"DYYYHideCollectButton" type:DYYYSettingItemTypeSwitch],
             [DYYYSettingItem itemWithTitle:@"隐藏分享按钮" key:@"DYYYHideShareButton" type:DYYYSettingItemTypeSwitch]
-        ],
-        section4
+        ]
     ];
 }
 
@@ -98,24 +111,29 @@
 
 - (void)addTitleGradientAnimation {
     CAGradientLayer *gradient = [CAGradientLayer layer];
-    gradient.colors = @[(id)[UIColor systemRedColor].CGColor, (id)[UIColor systemBlueColor].CGColor];
+    gradient.colors = @[(__bridge id)[UIColor systemRedColor].CGColor, (__bridge id)[UIColor systemBlueColor].CGColor];
     gradient.startPoint = CGPointMake(0, 0);
     gradient.endPoint = CGPointMake(1, 0);
     gradient.frame = CGRectMake(0, 0, 150, 30);
+    
     UIView *titleView = [[UIView alloc] initWithFrame:gradient.frame];
     [titleView.layer addSublayer:gradient];
+    
     UILabel *titleLabel = [[UILabel alloc] initWithFrame:titleView.bounds];
     titleLabel.text = self.title;
     titleLabel.textAlignment = NSTextAlignmentCenter;
     titleLabel.font = [UIFont boldSystemFontOfSize:20];
     titleLabel.textColor = [UIColor clearColor];
+    
     gradient.mask = titleLabel.layer;
     self.navigationItem.titleView = titleView;
+    
     CABasicAnimation *colorChange = [CABasicAnimation animationWithKeyPath:@"colors"];
-    colorChange.toValue = @[(id)[UIColor systemYellowColor].CGColor, (id)[UIColor systemGreenColor].CGColor];
+    colorChange.toValue = @[(__bridge id)[UIColor systemYellowColor].CGColor, (__bridge id)[UIColor systemGreenColor].CGColor];
     colorChange.duration = 2.0;
     colorChange.autoreverses = YES;
     colorChange.repeatCount = HUGE_VALF;
+    
     [gradient addAnimation:colorChange forKey:@"colorChangeAnimation"];
 }
 
@@ -131,13 +149,16 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     DYYYSettingItem *item = self.settingSections[indexPath.section][indexPath.row];
+    
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"SettingCell"];
     if (!cell) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"SettingCell"];
     }
+    
     cell.textLabel.text = item.title;
     cell.textLabel.textColor = [UIColor whiteColor];
     cell.backgroundColor = [UIColor colorWithRed:28/255.0 green:28/255.0 blue:29/255.0 alpha:1.0];
+    
     if (item.type == DYYYSettingItemTypeSwitch) {
         UISwitch *switchView = [[UISwitch alloc] init];
         [switchView setOn:[[NSUserDefaults standardUserDefaults] boolForKey:item.key]];
@@ -152,11 +173,13 @@
         textField.textAlignment = NSTextAlignmentRight;
         textField.backgroundColor = [UIColor darkGrayColor];
         textField.textColor = [UIColor whiteColor];
+        
         [textField addTarget:self action:@selector(textFieldDidChange:) forControlEvents:UIControlEventEditingDidEnd];
         textField.tag = indexPath.section * 1000 + indexPath.row;
         cell.accessoryView = textField;
     } else if (item.type == DYYYSettingItemTypeSpeedPicker) {
         cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+
         UITextField *speedField = [[UITextField alloc] initWithFrame:CGRectMake(0, 0, 80, 30)];
         speedField.text = [NSString stringWithFormat:@"%.2f", [[NSUserDefaults standardUserDefaults] floatForKey:@"DYYYDefaultSpeed"]];
         speedField.textColor = [UIColor whiteColor];
@@ -164,11 +187,14 @@
         speedField.backgroundColor = [UIColor clearColor];
         speedField.textAlignment = NSTextAlignmentRight;
         speedField.enabled = NO;
+        
         speedField.tag = 999;
         cell.accessoryView = speedField;
     }
+    
     return cell;
 }
+
 
 #pragma mark - UITableViewDelegate
 
@@ -184,6 +210,7 @@
     UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"选择倍速"
                                                                    message:nil
                                                             preferredStyle:UIAlertControllerStyleActionSheet];
+    
     NSArray *speeds = @[@0.75, @1.0, @1.5, @2.0, @2.5, @3.0];
     for (NSNumber *speed in speeds) {
         UIAlertAction *action = [UIAlertAction actionWithTitle:[NSString stringWithFormat:@"%.2f", speed.floatValue]
@@ -191,6 +218,7 @@
                                                       handler:^(UIAlertAction * _Nonnull action) {
             [[NSUserDefaults standardUserDefaults] setFloat:speed.floatValue forKey:@"DYYYDefaultSpeed"];
             [[NSUserDefaults standardUserDefaults] synchronize];
+            
             NSIndexPath *indexPath = [NSIndexPath indexPathForRow:2 inSection:0];
             UITableViewCell *cell = [self.tableView cellForRowAtIndexPath:indexPath];
             UITextField *speedField = [cell.accessoryView viewWithTag:999];
@@ -198,10 +226,14 @@
         }];
         [alert addAction:action];
     }
+    
     UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:nil];
     [alert addAction:cancelAction];
+    
     [self presentViewController:alert animated:YES completion:nil];
 }
+
+
 
 #pragma mark - Actions
 
@@ -210,12 +242,6 @@
     DYYYSettingItem *item = self.settingSections[indexPath.section][indexPath.row];
     [[NSUserDefaults standardUserDefaults] setBool:sender.isOn forKey:item.key];
     [[NSUserDefaults standardUserDefaults] synchronize];
-    
-    if ([item.key isEqualToString:@"DYYYEnableDanmuColor"]) {
-        self.isDanmuColorEnabled = sender.isOn;
-        [self setupSettingItems];
-        [self.tableView reloadData];
-    }
 }
 
 - (void)textFieldDidChange:(UITextField *)textField {
