fc21b068f432e948daeac9f77ce06ebb64b1a73d
Huangbai233 <1343309363@qq.com>
2025-03-17 02:29:59 +0800

    添加双击视频打开评论区

--- DYYYSettingViewController.m
+++ DYYYSettingViewController.m
@@ -180,7 +180,8 @@ typedef NS_ENUM(NSInteger, DYYYSettingItemType) {
             [DYYYSettingItem itemWithTitle:@"长按面板保存媒体" key:@"DYYYLongPressDownload" type:DYYYSettingItemTypeSwitch],
             [DYYYSettingItem itemWithTitle:@"移除评论实况水印" key:@"DYYYCommentLivePhotoNotWaterMark" type:DYYYSettingItemTypeSwitch],
             [DYYYSettingItem itemWithTitle:@"移除评论图片水印" key:@"DYYYCommentNotWaterMark" type:DYYYSettingItemTypeSwitch],
-            [DYYYSettingItem itemWithTitle:@"去评论表情包水印" key:@"DYYYFourceDownloadEmotion" type:DYYYSettingItemTypeSwitch]
+            [DYYYSettingItem itemWithTitle:@"去评论表情包水印" key:@"DYYYFourceDownloadEmotion" type:DYYYSettingItemTypeSwitch],
+            [DYYYSettingItem itemWithTitle:@"启用双击打开评论" key:@"DYYYEnableDoubleOpenComment" type:DYYYSettingItemTypeSwitch]
         ]
     ];
 }
