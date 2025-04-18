45a09758a8ca84f676591ae3b5e4aaf3af4688d2
zhaoyuedong <1151546095@qq.com>
2025-04-18 22:50:38 +0800

    Update AwemeHeaders.h
    添加DYYYGetBool宏定义

--- AwemeHeaders.h
+++ AwemeHeaders.h
@@ -1,6 +1,7 @@
 #import <UIKit/UIKit.h>
 #import <Photos/Photos.h>
 
+#define DYYYGetBool(key) [[NSUserDefaults standardUserDefaults] boolForKey:key]
 #define DYYY 100
 
 typedef NS_ENUM(NSInteger, MediaType) {
