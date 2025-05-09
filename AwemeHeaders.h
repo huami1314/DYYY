6a7586ad1d6b44a4d0fa213ba8cf365b06841d9f
lihyee <DayiIsabelle5@gmail.com>
2025-05-10 00:06:32 +0800

    更新 AwemeHeaders.h

--- AwemeHeaders.h
+++ AwemeHeaders.h
@@ -935,26 +935,19 @@ typedef NS_ENUM(NSInteger, MediaType) {
 @interface AWEDemaciaChapterProgressSlider : UIView
 @end
 
-@interface AWEFeedABSettings : NSObject
-+ (BOOL)enableHDRBrightnessOpt;
-+ (BOOL)hdrAutomaticIdentification;
+//HDR
+@interface AWEHDRModelManager : NSObject
++ (BOOL)enableVideoHDR;
++ (BOOL)useOneKeyHDR;
 @end
-@interface BDSimPlayerMediaViewController : UIViewController
-- (void)setEnableHDR:(BOOL)enable;
-- (void)setEnablePlayHDRMode;
+@interface VideoFrame : NSObject
 @end
-@interface AWEVideoPlayerConfiguration : NSObject
-+ (void)setHDRBrightnessStrategy:(id)strategy;
+@interface VEHDRDetectionUtils : NSObject
++ (BOOL)isHDRVideo:(id)video;
++ (id)detectionHDRType:(id)video;
 @end
-@interface BDSimPlayerBizConfig : NSObject
-- (BOOL)enableHDRBrightnessOpt;
-@end
-@interface AWEProtectEyesManager : NSObject
-- (void)setHDRlutImage:(id)image;
-@end
-@interface BDSimMediaPlayer : NSObject
-- (void)setEnableHDR:(BOOL)enable;
-- (void)setEnablePlayHDRMode;
+@interface BmfFilterSDR2HDR : NSObject
+- (VideoFrame *)process:(VideoFrame *)frame;
 @end
 
 @interface AWEABTestManager : NSObject
