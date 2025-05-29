148aa7b365c806c6e80323295a3bbad51d3d191f
lihyee <DayiIsabelle5@gmail.com>
2025-05-30 02:37:59 +0800

    更新 AwemeHeaders.h

--- AwemeHeaders.h
+++ AwemeHeaders.h
@@ -1089,3 +1089,6 @@ typedef NS_ENUM(NSInteger, MediaType) {
 - (void)handleLongPressLockedDoubleSpeedEnded:(id)arg1 gesture:(UIGestureRecognizer *)gesture;
 - (void)longPressSpeedControlDidChangeSpeed:(double)speed;
 @end
+
+@interface AWEPlayInteractionUserAvatarView : UIView
+@end
