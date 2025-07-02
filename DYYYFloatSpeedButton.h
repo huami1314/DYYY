7491385e3cccc4aa3c10ac12f31382f054cfb250
维他入我心 <lujialin@qhu.edu.cn>
2025-07-02 18:50:44 +0800

    Add fade behavior to floating speed button

--- DYYYFloatSpeedButton.h
+++ DYYYFloatSpeedButton.h
@@ -4,15 +4,17 @@
 @property(nonatomic, assign) CGPoint lastLocation;
 @property(nonatomic, weak) id interactionController;
 @property(nonatomic, assign) BOOL isLocked;
-@property(nonatomic, strong) NSTimer *firstStageTimer;
 @property(nonatomic, assign) BOOL justToggledLock;
 @property(nonatomic, assign) BOOL originalLockState;
 @property(nonatomic, assign) BOOL isResponding;
 @property(nonatomic, strong) NSTimer *statusCheckTimer;
+@property(nonatomic, strong) NSTimer *fadeTimer;
+@property(nonatomic, assign) CGFloat originalAlpha;
 - (void)saveButtonPosition;
 - (void)loadSavedPosition;
 - (void)resetButtonState;
 - (void)toggleLockState;
+- (void)resetFadeTimer;
 @end
 
 extern FloatingSpeedButton *getSpeedButton(void);
